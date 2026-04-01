6c0e37fa9ca847fd1d5253a193911e101ae8e139
FELIPE  ADRIAN LOURENÇO BARBISAN <0221432511007@fatectq.local>
2026-04-01 11:14:24 -0300

    create endpoint post and put

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -14,7 +14,7 @@ export async function Get(req,res){
 }
 export async function GetById(req,res){
     try{
-        const id = req.params.id
+        const id = req.params.id;
         const response = await myModel.GetById(id);
         if(response.message.toLowerCase == "success".toLowerCase)
             res.status(202).json(response);
@@ -25,9 +25,40 @@ export async function GetById(req,res){
         res.status(403).json({"message":"error","Error":err});
     }
 }
+export async function Post(req,res){
+    try{
+        const databody = req.body;
+        if(!databody)
+            res.status(400).json({"message":"error","Error":"Body Empy"});
+        const response = await myModel.Post(databody);
+        if(response.message.toLowerCase == "success".toLowerCase)
+            res.status(201).json(response);
+        else
+            res.status(400).json(response);
+    }catch (err){
+        console.log(err);
+        res.status(403).json({"message":"error","Error":err});
+    }
+}
+export async function Put(req,res){
+    try{
+        const id = req.params.id;
+        const databody = req.body;
+        if(!databody || !id)
+            res.status(400).json({"message":"error","Error":"Some thing is Empy"});
+        const response = await myModel.Put(id,databody);
+        if(response.message.toLowerCase == "success".toLowerCase)
+            res.status(201).json(response);
+        else
+            res.status(400).json(response);
+    }catch (err){
+        console.log(err);
+        res.status(403).json({"message":"error","Error":err});
+    }
+}
 export async function Delete(req,res){
     try{
-        const id = req.params.id
+        const id = req.params.id;
         const response = await myModel.Delete(id);
         if(response.message.toLowerCase == "success".toLowerCase)
             res.status(202).json(response);
